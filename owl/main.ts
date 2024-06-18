2b3302d809c2736eb138bd2389b6e7d0031d32fc
WAJEEHA QADIR ABBASI <160304483+IRmaju@users.noreply.github.com>
2024-06-18 22:28:16 +0500

    new

---
+++ owl/main.ts
@@ -0,0 +1,577 @@
+// // let a = "wajeeha";
+// // console.log(a.toLowerCase());
+
+// // console.log(a.toUpperCase());
+
+// // console.log(a.replace(/\b\w/g,(char) => char.toUpperCase()));
+
+
+// // console.log(" wajeeha said,\"I AM THE BEST.\"");
+
+// // let famousPerson = "ALI"
+
+// // let message = "once said, I AM BEST"
+
+// // // console.log(famousPerson ,message);
+
+// // let whitespaceName = "\n\t wajeeha Qadir Abbasi\t\n";
+// // console.log(whitespaceName)
+
+// // let withoutwhitespaceName= whitespaceName.trim();
+
+// // console.log(withoutwhitespaceName);
+
+
+// // console.log(4 +4);
+// // console.log(12-4);
+// // console.log(4*2);
+// // console.log(16/2);
+
+
+
+// // let b =35;
+// // let message = "my fav num is :";
+
+// // console.log(message, b);
+
+// // operation
+
+// // // addition
+// // console.log(4 +4);
+// // // subtraction
+// // // console.log(12-4);
+// // // // multipilication
+// // // console.log(4*2);
+// // // // division
+// // // console.log(16/2);
+
+// // // let frndName= ["ALI","WAJEEHA","HINA","HAMZA","usman"];
+
+// // // frndName.forEach(frndName=> console.log(frndName));
+
+// // // let frndName = ["ALI","WAJEEHA","HINA","HAMZA","USMAN"];
+
+// // // frndName.forEach(frndName => console.log(`Hello ${frndName}, H r u`))
+
+// // // let trnsportMode = ["BUS","CAR","JEEP","COROLLA"];
+
+// // // trnsportMode.forEach(trnsportMode => console.log(`I WOULD LIKE TO OWN ${trnsportMode}`));
+
+// // // let guestList =["ALI","NOMAN","HINA", "WAJEEHA","TARIQ"];
+// // // let noCome =guestList[0];
+
+// // // console.log(noCome , "nahi aa sakta");
+// // // guestList.splice(0, 1 ,"Aamir");
+
+// // // guestList.forEach(guest => console.log(`salaam ${guest}, would u like to dinner with us?`));
+
+
+// let guestList =["ALI","NOMAN","HINA", "WAJEEHA","TARIQ"];
+// // let noCome = guestList[0];
+
+// // console.log(noCome , "nahi aa sakta");
+
+// // guestList.splice(0, 1 ,"Aamir");
+
+// // console.log("Good News! we have found a big TABLE FOR DINNER");
+// // guestList.unshift(" Bilal");
+
+// // guestList.push("zain");
+
+// // let middleIndex : number = Math.floor(guestList.length /2);
+
+// // guestList.splice(middleIndex , 0,"Osma");
+
+// // console.log(" updated list ");
+
+// // guestList.forEach(oneguest => console.log(`salaam ${oneguest}, would u like dinner with us?`))
+
+// // console.log("unfortunately , the new Table wont arrive on Time , so I can invite  two Guest to dinner  with me")
+
+// // while(guestList.length > 2){
+//     // let removeGuest = guestList.pop();
+//     // console.log(`sorry, ${removeGuest} I cannot invite u for  to dinner`);
+
+//     // console.log("Invitation  to the last 2 Guest ");
+
+//     // guestList.forEach(lasttwo =>console.log(`luckyly ${lasttwo} , u r still invited`));
+
+//     // guestList.pop();
+//     // guestList.pop();
+
+//     // console.log("Empty List : ", guestList);
+
+
+// // }
+
+// // let contryVisit = ["CANADA", "BANKOKO","DENMARK"];
+// // console.log("original order:" ,contryVisit);
+
+// // console.log("Alphabatical Order:",[...contryVisit].sort());
+
+// // console.log("still in orinal order:", contryVisit);
+
+// // console.log("Reverse Order:" ,[...contryVisit].reverse());
+
+// // console.log("orinal Array Reversed:",contryVisit.reverse());
+
+// // console.log("back to original order:", contryVisit.reverse());
+
+// // // console.log("sorted in Alphabatical Order:",contryVisit.sort());
+
+// // // console.log("Original Array Reversed:", contryVisit.reverse());
+
+
+// // // let guestList :string[] =["ALI","NOMAN","HINA", "WAJEEHA","TARIQ"];
+
+// // // let Lengthguest = guestList.length ;
+// // // console.log( ` we r Inviting total ${Lengthguest} guest.`);
+
+
+// // // let progrmlang = ["javascript", "python" ,"Typscript","php" ];
+// // // console.log("List of progrmlang:");
+
+// // // progrmlang .forEach(language => console.log(language));
+// // // object
+
+// // interface itcorse {
+// //     corseNAme: string;
+// //     location: string;
+// //     onsiteStudent:number;
+// }
+
+// // let itcorse={
+// //     corseName : "typscript",
+// //     location: "governor House",
+// //     onsitesStudent : 50000
+
+// // };
+
+// // console.log(itcorse)
+
+// // let errorArray :string[] = ["A","B","C","D","E"];
+
+// // console.log(errorArray[3]);
+
+// let five = 5;
+
+// let ten =10;
+// let twenty =20;
+
+// console.log("test 1 : five is equal to 5?");
+// console.log(five == 5);
+
+
+// console.log("\ntest 2 : ten is equal to 10?");
+// console.log(ten == 10);
+
+
+// console.log("\ntest 3 : five is not equal to 1/0?");
+// console.log(5 != ten);
+
+
+// console.log("\ntest 4 : 10 is greater than 5?");
+// console.log(ten  > 5);
+
+
+// console.log("\ntest 5 : 5 is smaller than 10?");
+// console.log(5  < 10);
+
+// console.log("\ntest 6 : 10is smaller than 5?");
+// console.log(10  < 5);
+
+
+// console.log("\ntest 7 : 5 is equql  to 10?");
+// console.log(5 == 10);
+
+
+// console.log("\ntest 8 : 10 is not equql  to 10?");
+// console.log(10 != 10);
+
+// console.log("\ntest 9 : 5 is greater  than 10?");
+// console.log(5 >  10);
+
+
+
+// console.log("\ntest 10 : 100 is smaller  than 50?");
+// console.log(100 <  50);
+
+
+// let apple = "apple";
+// console.log("Is apple is equal to apple");
+// console.log(apple =="apple");
+
+// console.log("\nmainis apple is not equal to apple");
+
+// console.log(apple != "apple");
+
+// let uppercase = "APPLE";
+
+// console.log(" Is APPlE is equal to apple after converting to lowercase?");
+// console.log(upperCase.tolowerCase() =="apple");
+
+// console.log (" \n Is ten is equal to twenty?");
+// console.log(ten == twenty);
+
+// console.log (" \n Is ten is  not equal to twenty?");
+// console.log(ten != twenty);
+
+
+// console.log (" \n Is ten is  greater  than  zero?");
+// console.log(ten > 0);
+
+// console.log (" \n Is twenty is  less  than  10?");
+// console.log(20 < 10);
+
+
+// console.log (" \n Is ten is greater than or equal  5?");
+// console.log(10 >= 5);
+
+
+// console.log (" \n Is twenty is  less than or equal  10?");
+// console.log(20 <= 5);
+
+// console.log("\n twenty is not equal to 10 and twenty is greater than 10")
+// // console .log(twenty != 10 && twenty > 30);
+
+// // console.log("\n 20 is greater than  50 and twenty is equal  20")
+// // // console .log(twenty > 50 ||  twenty == 20);
+
+// // // console.log("\n 20 is greater than  50 and twenty is equal  20")
+// // // console .log(twenty > 50 ||  twenty != 20);
+
+// // // let frts = ["Apple", "banana", "orange"];
+// // // console.log("is orange include  in my fruit array");
+
+// // // console.log(frts.includes("orange"));
+
+// // // console.log(!frts .includes("orange"));
+
+
+
+// // let alincolor = "green";
+
+
+// // if (alincolor === "green"){
+// //     console.log(" u shot down  green alien u have earn 5 point");
+// // }
+// // else if(alincolor === "yellow"){
+// //     console.log("u shot down yellow alien u have earned 10 point")
+// // }
+// // else if (alincolor ==="red"){
+// //     console.log("u shot down red alin u have earn 15 point")
+// // }
+    
+// // let alinColor2 = " red ";
+
+// // if (alinColor2 === "green"){
+// //     console.log("u shot shot down alin u have earn 5 point")
+// // }
+// // else if (alinColor2 === "yellow"){
+// //     console.log("hot shot down alin u have earn 10 point")
+// // }
+
+// // else if (alinColor2 ==="red"){
+// //     console.log("hot shot down alin u have earn 15 point")
+// }
+
+
+// let alinColor3 = "red";
+
+// if (alinColor3 === "green"){
+//     console.log("u shot shot down alin u have earn 5 point")
+
+// }
+
+//  else if (alinColor3 === "yellow"){
+//     console.log("u shot shot down alin u have earn 10  point")
+
+//  }
+
+ 
+//  else if (alinColor3 === "red"){
+//     console.log("u shot shot down alin u have earn 15 point")
+//  }
+
+// let age = 64 ;
+
+// if (age < 2){
+//     console.log("u r a baby.")
+// }
+// else if (age >= 2 && age < 4 ){
+//     console.log(" u r a toddler.");
+
+// }
+// else if (age >= 4 &&  age< 13){
+//     console.log (" u r kid");
+// }
+
+// else if ( age >= 13  &&age < 20){
+//     console.log(" u r Teenager");
+// }
+// else if(age >= 20 && age< 65){
+//     console.log("u r adult")
+
+// // }
+// // else if (age >= 65){
+// //     console.log("u r Elder.")
+// // }
+
+
+// let fvrt_frt =["Apple","orange","Banana","Mango"];
+
+// if (fvrt_frt.includes("Mango")){
+//     console.log(" I realy Like Mango");
+
+// }
+
+// if (fvrt_frt.includes("orange")){
+//     console.log(" I realy Like orange");
+
+// }
+
+// if (fvrt_frt.includes("kiwi")){
+    
+//     console.log(" I realy Like kiwi");
+// }
+
+
+// if (fvrt_frt.includes("Banana")){
+    
+//     console.log(" I realy Like Banana");
+// }
+
+
+// if (fvrt_frt.includes("strawbery")){
+    
+//     console.log(" I realy Like strawbery");
+// }
+
+
+// if (fvrt_frt.includes("Apple")){
+    
+//     console.log(" I realy Like Apple");
+// }
+
+
+// let userName = ["wajeeha","ali","Noman","zain", "Admin"];
+
+//  userName.forEach(oneUser => {
+//     if (oneUser === "Admin"){
+
+//         console.log(`HEllo  ${oneUser}, would u like tpsee a status report?`)
+
+//     }else{
+//         console.log(`Hello ${oneUser } , thank u for logging in again`)
+//     }
+//  })
+
+
+//  let userNames = ["Ali","Noman","Ahad","Zeeshan","Admin"];
+//  userNames= []
+
+//  if (userNames.length === 0){
+//     console.log("ur Array in Empty WE need to find some users!")
+//  }else{
+    
+//  userNames.forEach(oneUser => {
+//     if (oneUser === "Admin"){
+
+//         console.log(`HEllo  ${oneUser}, would u like tpsee a status report?`)
+
+// //     }else{
+// //                 console.log(`Hello ${oneUser } , thank u for logging in again`)
+// //  }
+// // })
+// //  }
+
+
+// // let current_users = ["Ali","Noman","wajeeha","zain","Areeba"];
+// // let new_users = ["Hamza","Ayshea","Medad","Areeba" , "ALi"];
+
+// // new_users.forEach(new_one_user =>{
+
+// //     let our_condition = current_users.some ( current_one_user=> current_one_user.toLowerCase() === new_one_user .toLowerCase());
+
+// //     if (our_condition){
+// //         console.log(`sorry &{new_one_user} is already taken!`)
+// //     }else{
+// //         console.log(`This Usernmae &{new_one_user} is available`)
+// //     }
+// // })
+
+
+// let numbers =[1,2,3,4,5,6,7,8,9];
+
+// for( let oneNumber of numbers){
+//     let ordinalEnding :string;
+
+//     if(oneNumber === 1){
+//         ordinalEnding = "st"
+
+//     }else if( oneNumber === 2){
+//         ordinalEnding = "nd"
+//     }
+
+// else if (oneNumber === 3){
+//     ordinalEnding ="rd"
+// }
+// else{
+//     ordinalEnding ="th"
+// }
+
+// console.log(`${oneNumber}${ordinalEnding}`);
+
+
+// let pizza = ["fajita","tikka","cheese","Fajita"];
+
+// for(let onepizza of pizza){
+//     console.log(`i like ${onepizza} pizza`);
+// }
+
+// console.log ("pizza is Love")
+
+// console.log(" i eat pizza")
+
+// let petAnimals =["Cat","Dog","Rabbit"];
+
+// for(let onepet of petAnimals){
+//     console.log(`A ${onepet} would make a great pet.`);
+//     console.log("Any of these  animals would make great pet! \n");
+// }
+
+// console.log("Any of these  animals would make great pet! \n");
+
+
+
+//  function  myname(size :string,printmessage : string){
+//     console.log(`u selected${size} size shirt with ${printmessage} prints on shirts`)
+// }
+// myname("large", "codewithwajeeha");
+
+// // myname("small", "Wajeeha")
+
+// function yourshirt (size:string = "Large", printmessage:string = "i love Typscript"){
+//     console.log(`creating a ${size} shirt with the ${printmessage} print on message}`)
+// }
+// yourshirt();
+
+// yourshirt("Medium")
+
+// yourshirt("small" ,"i love javascript");
+
+// function city  (city : string ,country :string = "PAKISTAN"){
+// console.log(`${city} is in ${country}`);
+// }
+// // city("karachi");
+// // city("LAHORE");
+// // city("canada","GERMANY");
+
+
+// // function citycountry(city : string , country:string):string{
+// //     return`${city} , ${country}`;
+// // }
+// // citycountry("karachi","pakistan")
+
+// // console.log(citycountry("karachi","pakistan"));
+
+// // console.log(citycountry("TOKOYO","JAPAN"));
+// // console.log(citycountry("BERLIN ","GERMANY"));
+
+// // function car (Name : string ){
+// //      console.log(`your name is ${Name} Abbasi.`);
+// //  }
+// //   car ("wajeeha")
+
+// // myName("wajeeha");
+
+// // console.log(myName("Wajeeha"));
+// // console.log(myName("ALI"));
+
+// // console.log(myName("SANA"));
+
+
+// // function myName (Name  :string) : string{
+// // return `${Name}`
+// // myName("wajeeha");
+// // }
+
+// // console.log("wajeeha")
+
+
+// function make_album(artist_name : string , album_title: string, tracks ? : number ){
+    
+//     let album :{artist :string ,title :string , tracks?: number}={
+//         artist :artist_name,
+//         title: album_title,
+
+    
+// };
+// if (tracks !== undefined){
+    // album.tracks = tracks;
+
+//     return album;
+
+// }
+
+//    let album1 =make_album ("artist no 1 ", "album title 1");
+//    let album2 = make_album("usman","Album Title 2");
+//    let album3 = make_album("noman" , "Album title 3 ",10);
+
+//    console.log(album1);
+//    console .log(album2);
+//    console.log(album3);
+
+
+// function show_magicians (magicians :string[]){
+//      magicians.forEach( name => console.log(name));
+
+// }
+
+// function make_great(magicians : string[]){
+//    return magicians.map(name => `THE GREAT ${name}`);
+
+// }
+
+// let magician_names = ["harry", "poter","aLi"];
+
+//  let great_magicians= make_great(magician_names);
+
+// show_magicians(great_magicians);
+
+// console.log(show_magicians)
+
+
+
+// function makeSandwhich (...items : string[]){
+//     console.log(" Making sandwhich with following items :\n");
+//     items.forEach(sglItem => console.log(sglItem));
+//     console.log("\n Now Enjoy  your Sandwich");
+
+// }
+
+// makeSandwhich("chicken","Mayo" ,"garlic","bread");
+
+// makeSandwhich("Bread" ,"Butter");
+
+// makeSandwhich("letuce","cheez","Egg","Mayo","Butter","TOmato")
+
+
+function create_car(manufacturer , model, ...options){
+    let car = {
+        manufacture :manufacturer,
+        model:model
+};
+
+options.forEach(option =>{
+    let[key , value] = option.split(":");
+    car.[key.trim()] = value.trim();
+});
+return car;
+}
+
+let my_car = create_car("Toyota","haris","yasir" ,"sunroof :true")
+
+console.log(my_car)
+
+
